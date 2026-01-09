d1735d48881faa10b771e81a376791d13b417b59
Justin-Bonilla <177785354+Justin-Bonilla@users.noreply.github.com>
2026-01-08 23:10:23 -0700

    Homework1 have no clue how to set up the file so i just made a generic one and put my expressions in

---
+++ hw1/Homework1.srcs/sources_1/new/homework1basic.vhd
@@ -0,0 +1,43 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 01/08/2026 10:51:41 PM
+-- Design Name: 
+-- Module Name: homework1basic - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity homework1basic is
+--  Port ( );
+end homework1basic;
+
+architecture Behavioral of homework1basic is
+
+begin
+
+
+end Behavioral;
